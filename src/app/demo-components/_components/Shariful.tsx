47bda2a848eb3fe245b8dd97de6f9e499c72838c
Shariful525 <sharifulafsar0@gmail.com>
2024-09-17 19:57:49 +0600

    primary and secondary color added on globals.css

--- src/app/demo-components/_components/Shariful.tsx
+++ src/app/demo-components/_components/Shariful.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Shariful = () => {
-  return <div>Here is shariful</div>;
+  return (
+    <div className="space-y-4">
+      <div className="w-36 h-12 bg-gradient-primary rounded-md"></div>
+      <div className="w-36 h-12 bg-gradient-secondary-light rounded-md"></div>
+      <div className="w-36 h-12 bg-gradient-secondary rounded-md"></div>
+      <div className="w-36 h-12 bg-gradient-primary-light rounded-md"></div>
+    </div>
+  );
 };
 
 export default Shariful;
