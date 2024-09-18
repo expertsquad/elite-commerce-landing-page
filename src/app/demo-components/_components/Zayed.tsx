cebb25befdcf8ce08248e5db8f20096fa6b3547d
zayedhossain120 <zayedhossain120@gmail/com>
2024-09-18 12:27:52 +0600

    added some updated

--- src/app/demo-components/_components/Zayed.tsx
+++ src/app/demo-components/_components/Zayed.tsx
@@ -1,10 +1,11 @@
 import BgNionLine from "@/components/Shared/BgNionLine";
+import NeonLineBorderDiv from "@/components/ui/NeonLineBorderDiv";
 import React from "react";
 
 const Zayed = () => {
   return (
     <div>
-      <BgNionLine />
+      <NeonLineBorderDiv />
     </div>
   );
 };
