6ea38440cfcedb09176919da537b143f73a5873e
Shariful525 <sharifulafsar0@gmail.com>
2024-09-28 20:05:55 +0600

    updated privacy policy and refund policy

--- src/app/refund-policy/page.tsx
+++ src/app/refund-policy/page.tsx
@@ -4,7 +4,7 @@ const RefundPolicy = () => {
   return (
     <div className="main-container h-[calc(100vh-75px)] overflow-y-auto scrollbar-y-remove">
       <h1 className="text-center font-bold [font-size:_clamp(30px,2.5vw,50px)]">
-        Refund Policy Of Elite Commerce
+        Coming Soon ....
       </h1>
     </div>
   );
